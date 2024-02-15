18a693277f4135bafa3547ea63e5753af9427c2f
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2024-02-15 10:27:03 +0100

    Día 45, Navegación y paso de datos entre pantallas, 125, 126 y 127

---
+++ Proyectos/Navegacion/Navegacion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navegacion
+//
+//  Created by Esteban Perez Castillejo on 15/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
