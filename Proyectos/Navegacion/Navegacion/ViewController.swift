a3347da9c2a0a1f85372c6a6aa5b761679abdf7f
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2024-02-16 18:01:08 +0100

    Día 46, Navegación y paso de datos entre pantallas, 128 y 129

--- Proyectos/Navegacion/Navegacion/ViewController.swift
+++ Proyectos/Navegacion/Navegacion/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func myButtonBlack(_ sender: Any) {
+        print("I´m Button Black")
+        performSegue(withIdentifier: "VCBlack", sender: self)
+    }
+    
+    
+    @IBAction func myButtonPink(_ sender: Any) {
+        print("I´m Button Pink")
+        performSegue(withIdentifier: "VCPink", sender: self)
+    }
+    
+    
+    @IBAction func myButtonYelow(_ sender: Any) {
+        print("I´m Button Yelow")
+        performSegue(withIdentifier: "VCYelow", sender: self)
+    }
+    
 }
 
