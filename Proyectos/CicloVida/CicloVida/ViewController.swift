c0c451c4584c99d0a7b2cf1eca82b3e5b499993b
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2024-02-29 11:07:17 +0100

    Día 59, Sección 18, Ciclo de vida del ViewController y App,  155  y  156

---
+++ Proyectos/CicloVida/CicloVida/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CicloVida
+//
+//  Created by Esteban Perez Castillejo on 29/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Estoy en VC1 y se activo -> \(#function)")
+    }
+
+
+}
+
