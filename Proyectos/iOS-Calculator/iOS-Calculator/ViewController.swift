6cdf54cde1295d984aeeae59459037e290b713ad
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2024-03-09 22:26:38 +0100

    Sección 20, Crear app para iPhone y iPad, desde cero ,  175 y 176

---
+++ Proyectos/iOS-Calculator/iOS-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Calculator
+//
+//  Created by Esteban Perez Castillejo on 9/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
