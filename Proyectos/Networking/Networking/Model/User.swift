941077ed823b67e4ffc29e1c15c36651be0e41b4
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2024-03-25 10:22:33 +0100

    Día 84, Sección 22, API REST: Comunicación HTTP con S.web  211 y 212

--- Proyectos/Networking/Networking/Model/User.swift
+++ Proyectos/Networking/Networking/Model/User.swift
@@ -8,13 +8,16 @@
 import Foundation
 
 struct Users {
+    
     let users: User?
 }
 
 struct User {
+    
     let id: Int?
     let name: String?
     let email: String?
     let gemder: String?
     let status: String?
+    
 }
