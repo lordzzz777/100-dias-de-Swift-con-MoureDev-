75a66e90f7ad784a94e220f31c6ff3ff77d3a48e
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2024-03-26 12:10:11 +0100

    Día 85, Sección 22, API REST: Comunicación HTTP con S.web  213 y 214

--- Proyectos/Networking/Networking/Server/NetworkingProvider.swift
+++ Proyectos/Networking/Networking/Server/NetworkingProvider.swift
@@ -11,4 +11,18 @@ import Alamofire
 final class NetworkingProvider{
     
     static let shared = NetworkingProvider()
+    
+    private let kBaseUrl = "https://gorest.co.in/public-api/"
+    private let kStatusOk = 200...299
+    
+    func getUser(id: Int){
+        let url = "\(kBaseUrl)users/\(id)"
+        AF.request(url,method: .get).validate(statusCode: kStatusOk).responseDecodable (of: Users.self){ response in
+            if let user = response.value?.dataUser{
+                print(user) 
+            }else{
+                print(response.error ?? "No error" )
+            }
+        }
+    }
 }
--- Proyectos/Networking/Networking/View/ViewController.swift
+++ Proyectos/Networking/Networking/View/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var getUser: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getUserAction(_ sender: Any) {
+        NetworkingProvider.shared.getUser(id: 6805326)
+    }
+    
 }
 
